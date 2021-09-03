2d7fed3ea0767a1d2ab58dd5af685860b147ff9e
Eduardo Dantas <edantasn@gmail.com>
2021-09-03 18:11:35 -0300

    ...

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') - {{ config('app.name') }}</title>
+    <link rel="stylesheet" href="{{ url('css/app.css') }}">
 </head>
-<body>
+<body class="bg-gray-50">
     <header>
         
     </header>
-    <div class="content">
+    <div class="container mx-auto py-8">
         @yield('content')
     </div>
     <footer>
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -5,9 +5,11 @@
 @section('content')
     <a href="{{ route('posts.index') }}">Voltar</a><hr>
 
-    <h1>Cadastrar novo Post</h1>
+    <h1 class="text-center text-3x1 uppercase font-black my-4">Cadastrar novo Post</h1>
 
-    <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
-        @include('admin.posts._partials.form')
-    </form>
+    <div class="w-11/12 p-12 bg-white sm:w-8/12 md:w-1/2 lg:w-5/12 mx-auto">
+        <form action="{{ route('posts.store') }}" method="post" enctype="multipart/form-data">
+            @include('admin.posts._partials.form')
+        </form>
+    </div>
 @endsection
